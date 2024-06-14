1bbf595f3ff32e5a8ddf4ab2998cd9504d3ee039
Anderson Tonin Thums <atthums@ucs.br>
2024-06-14 19:41:23 -0300

    Scroll portfólio

---
+++ portfolio/reveal.js
@@ -0,0 +1,98 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+/* Topo do site */
+revelar.reveal('.efeito-txt-topo', 
+{
+    duration: 1000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo', 
+{
+    duration: 1000,
+    distance: '90px',
+    delay: 500
+})
+
+/* Titulos */
+revelar.reveal('.titulo', 
+{
+    duration: 1000,
+    distance: '40px'
+})
+
+/* Habilidades */
+revelar.reveal('.efeito-especialidades1', 
+{
+    duration: 1000,
+    distance: '40px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-especialidades2', 
+{
+    duration: 1000,
+    distance: '40px',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-especialidades3', 
+{
+    duration: 1000,
+    distance: '40px',
+    delay: 1500
+})
+
+/* Sobre */
+revelar.reveal('.efeito-img-sobre', 
+{
+    duration: 1000,
+    distance: '40px',
+    delay: 1000,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-txt-sobre', 
+{
+    duration: 1000,
+    distance: '40px',
+    delay: 500,
+    origin: 'right'
+})
+
+/* Formulário */
+revelar.reveal('.efeito-nome-form', 
+{
+    duration: 1000,
+    distance: '40px',
+    delay: 500
+})
+
+revelar.reveal('.efeito-email-form', 
+{
+    duration: 1000,
+    distance: '40px',
+    delay: 1000
+})
+
+revelar.reveal('.efeito-cel-form', 
+{
+    duration: 1000,
+    distance: '40px',
+    delay: 1500
+})
+
+revelar.reveal('.efeito-txt-form', 
+{
+    duration: 1000,
+    distance: '40px',
+    delay: 2000
+})
+
+revelar.reveal('.efeito-btn-form', 
+{
+    duration: 1000,
+    distance: '40px',
+    delay: 2500
+})
